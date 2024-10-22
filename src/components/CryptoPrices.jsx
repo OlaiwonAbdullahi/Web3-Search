8f7dea69a77cce0bbf74ecc1fde37063d6de3bcf
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-22 11:35:51 +0100

    update Tracker.jsx

---
+++ src/components/CryptoPrices.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CryptoPrices = () => {
+  return <div>CryptoPrices</div>;
+};
+
+export default CryptoPrices;
--- src/pages/Tracker.jsx
+++ src/pages/Tracker.jsx
@@ -3,11 +3,14 @@ import SearchHeader from "../components/SearchHeader";
 const Tracker = () => {
   return (
     <div className=" bg-primary h-screen">
-      <SearchHeader>
-        <span className=" text-secondary">C</span>rypto{" "}
-        <span className=" text-secondary">P</span>rice{" "}
-        <span className=" text-secondary">T</span>racker
-      </SearchHeader>
+      <div className="">
+        <SearchHeader>
+          <span className=" text-secondary">C</span>rypto{" "}
+          <span className=" text-secondary">P</span>rice{" "}
+          <span className=" text-secondary">T</span>racker
+        </SearchHeader>
+      </div>
+      <div className=""></div>
     </div>
   );
 };
