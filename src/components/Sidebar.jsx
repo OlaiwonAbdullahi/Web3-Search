b5a033a40e06055d033a40a0063260538a41aa1d
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-22 10:28:24 +0100

    update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
-import { CiFolderOn, CiGlobe } from "react-icons/ci";
+import { CiFolderOn, CiGlobe, CiSettings, CiUser } from "react-icons/ci";
 import { IoTrendingUp } from "react-icons/io5";
+import { SlSocialGithub } from "react-icons/sl";
 
 const Sidebar = () => {
   return (
-    <div className="bg-primary1 w-14 h-screen">
+    <div className="bg-primary1 w-14 h-screen flex flex-col justify-between">
       <div className="  text-text p-2 flex flex-col items-center justify-around h-1/3 ">
         <div className=" bg-primaryHover p-1 rounded-lg">
           <CiGlobe className=" size-8" />
@@ -13,6 +14,17 @@ const Sidebar = () => {
         </div>
         <div className=" bg-primaryHover p-1 rounded-lg">
           <CiFolderOn className=" size-8" />
+        </div>
+      </div>
+      <div className=" p-2 flex flex-col items-center justify-around gap-2">
+        <div className="text-text">
+          <CiUser className=" size-6" />
+        </div>
+        <div className="text-text">
+          <CiSettings className=" size-6" />
+        </div>
+        <div className="text-text">
+          <SlSocialGithub className=" size-6" />
         </div>
       </div>
     </div>
