7e2d60be00a871c1a1c5ba7d51825cc02b5f97cb
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-23 12:34:39 +0100

    update MessagingInput.jsx

--- src/components/MessagingInput.jsx
+++ src/components/MessagingInput.jsx
@@ -1,18 +1,34 @@
 import { AiOutlineSend } from "react-icons/ai";
+import { useState } from "react";
 
-const MessagingInput = () => {
+const MessagingInput = ({ sendMessage }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    if (inputValue.trim()) {
+      sendMessage(inputValue, "User");
+      setInputValue("");
+    }
+  };
+
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-primary flex justify-center  p-2">
-      <div className="flex gap-2 items-center w-2/3 border border-secondary justify-between p-1.5 rounded-lg">
+    <div className="fixed bottom-0 left-0 right-0 bg-primary flex justify-center p-2">
+      <form
+        className="flex gap-2 items-center w-2/3 border border-secondary justify-between p-1.5 rounded-lg"
+        onSubmit={handleSendMessage}
+      >
         <input
           type="text"
           placeholder="Ask Anything"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
           className="w-full p-1 bg-transparent text-lg focus:outline-none text-text"
         />
-        <button className="flex items-center">
+        <button type="submit" className="flex items-center">
           <AiOutlineSend className="size-8 text-secondary" />
         </button>
-      </div>
+      </form>
     </div>
   );
 };
