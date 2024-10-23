b0e80fe46dc7d3b85c2dac9ac730b920b6d0a233
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-23 11:53:10 +0100

    build(deps): update package-lock.json, package.json, MessagingInput.jsx and Sidebar.jsx

--- src/components/MessagingInput.jsx
+++ src/components/MessagingInput.jsx
@@ -1,10 +1,17 @@
 import { AiOutlineSend } from "react-icons/ai";
+
 const MessagingInput = () => {
   return (
-    <div>
-      <div className=" flex items-center w-2/3 border border-secondary justify-between p-2 ro">
-        <input type="text" className="" />
-        <AiOutlineSend className="size-8" />
+    <div className="fixed bottom-0 left-0 right-0 bg-primary flex justify-center  p-2">
+      <div className="flex gap-2 items-center w-2/3 border border-secondary justify-between p-1.5 rounded-lg">
+        <input
+          type="text"
+          placeholder="Ask Anything"
+          className="w-full p-1 bg-transparent text-lg"
+        />
+        <button className="flex items-center">
+          <AiOutlineSend className="size-8 text-secondary" />
+        </button>
       </div>
     </div>
   );
---
+++ src/components/PriceMarquee.jsx
@@ -0,0 +1,5 @@
+const PriceMarquee = () => {
+  return <div>PriceMarquee</div>;
+};
+
+export default PriceMarquee;
