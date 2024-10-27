98371b71fc43a2054f2baf326a9f9cc7e2ec054c
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-27 11:39:01 +0100

    update Search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -29,7 +29,7 @@ const Search = () => {
 
   return (
     <div className="bg-primary min-h-screen opacity-95 flex flex-col gap-6 p-4">
-      {/* PriceMarquee */}
+      {/* PriceMarquee 
       <div className="w-full mb-4">
         <PriceMarquee />
       </div>
@@ -37,8 +37,14 @@ const Search = () => {
       {/* Search Header */}
       <div className="text-center">
         <SearchHeader>
-          <span className="text-secondary text-4xl md:text-5xl">W</span>eb3{" "}
-          <span className="text-secondary text-4xl md:text-5xl">S</span>earch
+          <div className=" flex gap-2 mx-auto text-center justify-center">
+            <p className="text-3xl">
+              <span className="text-secondary text-4xl">W</span>eb3{" "}
+            </p>
+            <p className="text-3xl">
+              <span className="text-secondary text-4xl">S</span>earch
+            </p>
+          </div>
         </SearchHeader>
       </div>
 
