8f94346289f88043783a2ad5605e00fa7965ddb1
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-27 11:59:42 +0100

    update App.jsx and MobileNavBar.jsx

--- src/components/MobileNavBar.jsx
+++ src/components/MobileNavBar.jsx
@@ -4,7 +4,7 @@ import { SlSocialGithub } from "react-icons/sl";
 import { Link } from "react-router-dom";
 const MobileNavBar = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-primary1 rounded-full mb-3 mx-2 flex justify-around w-full text-text p-2 z-20 md:hidden">
+    <div className="fixed bottom-0 left-0 right-0 bg-primary1 rounded-full mb-3 flex justify-around w-full text-text p-2 z-20 md:hidden">
       <Link to="/">
         <div className=" bg-primaryHover text-secondary p-1 rounded-lg cursor-pointer">
           <CiGlobe className=" size-8 text-secondary" />
