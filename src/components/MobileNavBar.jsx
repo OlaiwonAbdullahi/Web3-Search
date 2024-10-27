fd13ac2342c1fea9003e60beb578ff1869dba8d8
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-27 11:43:33 +0100

    update MobileNavBar.jsx

--- src/components/MobileNavBar.jsx
+++ src/components/MobileNavBar.jsx
@@ -2,15 +2,15 @@ import { CiGlobe, CiUser } from "react-icons/ci";
 import { IoTrendingUp } from "react-icons/io5";
 const MobileNavBar = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-primary flex justify-around w-full text-text p-2">
+    <div className="fixed bottom-0 left-0 right-0 bg-primary flex justify-around w-full text-text p-2 z-20">
       <div className="">
-        <CiGlobe />
+        <CiGlobe className=" size-8 text-secondary" />
       </div>
       <div className="">
-        <IoTrendingUp />
+        <IoTrendingUp className=" size-8 text-secondary" />
       </div>
       <div className="">
-        <CiUser />
+        <CiUser className=" size-8 text-secondary" />
       </div>
     </div>
   );
